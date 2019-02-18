b8f78f2f2e66e75c9104d2a1b18c3bcd0a7aa1fb
thedemonium <demonium@komposerv.ru>
2019-02-18 14:18:09 +0300

    Add README.md file

--- DZ4/untitled.sh
+++ DZ4/untitled.sh
@@ -1,3 +1,35 @@
 #!/usr/bin/env bash
 
 
+# Services to check
+SERVICES=('mysql' 'nginx' 'php-fpm')
+
+# Contact email
+EMAIL="info@example.com"
+
+# Hostname (automatic)
+HOST=$HOSTNAME
+
+# Subject
+STATUS="Service-Status {$HOSTNAME}"
+
+
+# - stop editing - #
+
+
+for SERVICE in "${SERVICES[@]}"; do
+        var=`ps -eaf | grep ${SERVICE} | grep -v grep | wc -l`
+
+        if [ "$var" -lt "1" ]; then
+                service ${SERVICE} restart
+
+                if [ "$var" -lt "1" ]; then
+                        echo "${HOST}: ${SERVICE} konnte nicht neugestartet werden!" | mail -s "$STATUS" $EMAIL
+                else
+                        echo "${HOST}: ${SERVICE} wurde erfolgreich neugestartet." | mail -s "$STATUS" $EMAIL
+                fi
+        else
+                echo "${SERVICE} laeuft bereits."
+        fi
+
+done
