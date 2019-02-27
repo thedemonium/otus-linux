277c231ce897a69a76c1b84e4f7f976c29cf0146
thedemonium <demonium@komposerv.ru>
2019-02-27 16:07:12 +0300

    Add DZ5

--- DZ5/ionice.sh
+++ DZ5/ionice.sh
@@ -1,38 +1,4 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD:DZ4/untitled.sh
 
 
-# Services to check
-SERVICES=('mysql' 'nginx' 'php-fpm')
 
-# Contact email
-EMAIL="info@example.com"
-
-# Hostname (automatic)
-HOST=$HOSTNAME
-
-# Subject
-STATUS="Service-Status {$HOSTNAME}"
-
-
-# - stop editing - #
-
-
-for SERVICE in "${SERVICES[@]}"; do
-        var=`ps -eaf | grep ${SERVICE} | grep -v grep | wc -l`
-
-        if [ "$var" -lt "1" ]; then
-                service ${SERVICE} restart
-
-                if [ "$var" -lt "1" ]; then
-                        echo "${HOST}: ${SERVICE} konnte nicht neugestartet werden!" | mail -s "$STATUS" $EMAIL
-                else
-                        echo "${HOST}: ${SERVICE} wurde erfolgreich neugestartet." | mail -s "$STATUS" $EMAIL
-                fi
-        else
-                echo "${SERVICE} laeuft bereits."
-        fi
-
-done
-=======
->>>>>>> e3dae234c2d0fc7c73e646ec8a4ea5eda45a6c65:DZ5/ionice.sh
--- DZ5/ps_ax.sh
+++ DZ5/ps_ax.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
-
-pids=`ls /proc/ | grep [0-9]`
+pids=`ls /proc/ | grep [0-9] | sort -n`
 
 for pid in $pids
   do
-  	echo ""
+  	DIRECTORY="/proc/$pid"
+    if [ -d "$DIRECTORY" ]; then
+    echo ""
 	pidproc=`cat /proc/$pid/status | grep -w "Pid:" | awk '{print $2}'`
 	ttyproc=`readlink /proc/$pid/fd/0`
 	statproc=`cat /proc/$pid/status | grep -w "State:" | awk '{print $2}'`
 	timeproc=`awk '{print $14+$15}' /proc/$pid/stat`
-	cmdproc=`cat /proc/$pid/cmdline`
-	printf "$pidproc $ttyproc $statproc $timeproc $cmdproc"
+	#cmdproc=`cat /proc/$pid/cmdline`
+	cmdproc=$(tr -d '\0' </proc/$pid/cmdline)
+	printf "$pidproc | $ttyproc | $statproc | $timeproc | $cmdproc "
+    fi
   done
+
+
 
